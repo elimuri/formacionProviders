cecc188fc2b66cb7bf896f228351a30ac09393c7
Elisa <elisa.oreja@biko2.com>
2024-06-07 17:34:22 +0100

    feat: mete a todas las rutas el contexto

--- src/views/AboutPage.tsx
+++ src/views/AboutPage.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useUserContext } from "../hooks/useUserContext";
 
 export const AboutPage: React.FC = () => {
+  const { gif } = useUserContext();
+
   return (
     <>
       <h1>AboutPage</h1>
       <hr />
+
+      <img src={gif} alt="" />
     </>
   );
 };
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useUserContext } from "../hooks/useUserContext";
 
 export const HomePage: React.FC = () => {
+  const { gif } = useUserContext();
+
   return (
     <>
       <h1>HomePage</h1>
       <hr />
+
+      <img src={gif} alt="" />
     </>
   );
 };
