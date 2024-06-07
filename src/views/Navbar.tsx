bc78079e5510864326fe549681be8965fe0bff56
Elisa <elisa.oreja@biko2.com>
2024-06-07 16:59:26 +0100

    feat: da algo de estilos a los links

--- src/views/Navbar.tsx
+++ src/views/Navbar.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const Navbar: React.FC = () => {
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
-    </>
+    <NavBarOptions>
+      <RouteLink to="/">Home</RouteLink>
+      <RouteLink to="/about">About</RouteLink>
+      <RouteLink to="/login">Login</RouteLink>
+    </NavBarOptions>
   );
 };
+
+const NavBarOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-left: 40px;
+`;
+
+const RouteLink = styled(Link)`
+  margin-bottom: 5px;
+
+  font-weight: 600;
+
+  &:hover {
+    color: green;
+  }
+`;
